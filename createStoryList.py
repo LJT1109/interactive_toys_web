a780748e1725bd4c5567070ee5c29b508e986e5f
LJT1109 <105228706+LJT1109@users.noreply.github.com>
2023-12-20 02:01:39 +0800

    b

---
+++ createStoryList.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+def generate_story_list(directory):
+    stories = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt") and not filename.startswith('.'):
+            name, age = filename.rsplit('.', 1)[0].split('_', 1)
+            stories.append({"name": name, "age": age})
+
+    return stories
+
+# 指定存放故事的目錄
+story_dir = './Story'
+
+# 生成故事列表
+story_list = generate_story_list(story_dir)
+
+# 將故事列表寫入 JSON 文件
+with open('storyList.json', 'w', encoding='utf-8') as f:
+    json.dump(story_list, f, ensure_ascii=False, indent=4)
+
+print("storyList.json has been created.")
--- getmenu.php
+++ getmenu.php
@@ -1,28 +1,22 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-#get files in ./Story and format as json , json format is age name link
-$dir = "./Story";
-$dh  = opendir($dir);
-while (false !== ($filename = readdir($dh))) {
-    $files[] = $filename;
-}
-sort($files);
-
-#format as json
-foreach ($files as $filename) {
-    if($filename == "." || $filename == ".." || $filename == "getmenu.php")
-        continue;
-
-    $file = fopen("./Story/$filename", "r");
-    #split filename to age and name and ignore file extension
-    $filename = explode(".", $filename);
-    $filename = $filename[0];
-    $filename = explode("_", $filename);
-    $name = $filename[0];
-    $age = $filename[1];
-    $json[] = array("age" => $age, "name" => $name);
-    fclose($file);
-}
-echo json_encode($json, JSON_UNESCAPED_UNICODE);
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Story List</title>
+    <script>
+        function loadStories() {
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'storyList.json', true);
+            xhr.onload = function() {
+                if (this.status == 200) {
+                    var json = JSON.parse(this.responseText);
+                    document.getElementById('stories').innerHTML = JSON.stringify(json, null, 2);
+                }
+            };
+            xhr.send();
+        }
+    </script>
+</head>
+<body onload="loadStories()">
+    <pre id="stories"></pre>
+</body>
+</html>
